f3f55942f68cbc4d8e5a86628dca762b9f8893ff
Dave <Dave@BATMANV7>
2017-09-19 19:02:15 -0500

    Add project files.

---
+++ UmbracoLearning/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UmbracoLearning.Startup))]
+namespace UmbracoLearning
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
